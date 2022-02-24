5d50aa82d483397a6297baeab95b19a2e96a9904
Rares Matei <matei.rar@gmail.com>
2022-02-24 19:50:46 +0000

    add configuration

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import {LibBuildableModule} from '@dependson-config/lib-buildable'
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LibBuildableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
